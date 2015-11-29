9b1f45ef44dd6ba59fff5f58170692e164106459
Daniel Villanueva <daniel.jose@villanueva.com.ve>
2015-11-29 12:59:16 -0430

    Add loading Data

--- components/NoticesApp.js
+++ components/NoticesApp.js
@@ -11,22 +11,25 @@ import $ from 'jquery';
 export default class NoticesApp extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state 	= { notices: [], noticeActive: null };
+		this.state 	= { notices: [], dataStatus: null };
 		this.user 	= uid(10);
 		this.upData = this.upData.bind(this);
 	}
-	componentWillMount() {
+	componentDidMount() {
 		this.upData();
 	}
 	upData() {
-		$.get('/notices', (data) => {
-			this.setState({ notices: data });
+		this.setState({ notices: [], dataStatus: "loading" });
+		$.getJSON('/notices').done((data) => {
+			this.setState({ notices: data, dataStatus: "done" });
+		}).fail(() => {
+			this.setState({ notices: [], dataStatus: "fail" });
 		});
 	}
 	render() {
-		return <div className="pokeapp">
+		return <div>
 			<NoticesHeader upData={this.upData} />
-			<NoticesBody notices={this.state.notices} />
+			<NoticesBody notices={this.state.notices} dataStatus={this.state.dataStatus} />
 		</div>
 	}
 }
--- components/NoticesBody.js
+++ components/NoticesBody.js
@@ -7,20 +7,26 @@ import Article from './Article';
 import ArticleDetails from './ArticleDetails';
 
 export default class NoticesBody extends React.Component {
-  returnRender() {
-	  
+  renderBody() {
+		if (this.props.dataStatus == "done")				
+			return this.props.notices.map((notice) => {
+				  return <div>
+						 <Article notice={notice} />
+						 <ArticleDetails notice={notice} />
+					</div>
+			})
+		else if (this.props.dataStatus == "loading")
+			return <div className="cssload-thecube">
+					<div className="cssload-cube cssload-c1"></div>
+					<div className="cssload-cube cssload-c2"></div>
+					<div className="cssload-cube cssload-c4"></div>
+					<div className="cssload-cube cssload-c3"></div>
+				</div>
   }
   render() {
     return <div className="content">
 				<section className="row">
-				{
-					this.props.notices.map((notice) => {
-						  return <div>
-								 <Article notice={notice} />
-								 <ArticleDetails notice={notice} />
-							</div>
-					})
-				}
+				{ this.renderBody() }
 				</section>
 			</div>
   }
